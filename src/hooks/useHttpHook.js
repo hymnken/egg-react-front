97be70af3abede3c656a4fb4e73c19743d611622
hymnken <hymnken@yeah.net>
2023-04-30 23:15:26 +0800

    1

--- src/hooks/useHttpHook.js
+++ src/hooks/useHttpHook.js
@@ -1,16 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Http } from '@/utils';
-
 export default function useHttpHook({
   url,
   method = 'post',
   headers,
   body = {},
   watch = []
-}) {
+})
+{
   const [result, setResult] = useState();
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     Http({
       url,
@@ -21,6 +20,5 @@ export default function useHttpHook({
       setLoading
     });
   }, watch);
-
   return [result, loading];
 }
